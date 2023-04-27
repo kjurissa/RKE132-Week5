4d6e6e3719b527b1717d4a84fd0d36c437e5074c
Kärt Kristina <kjurissa@tktk.ee>
2023-04-27 15:32:24 +0300

    week 5

---
+++ ANDRanges/Program.cs
@@ -0,0 +1,34 @@
+﻿//temp >= o - väga külm
+//temp >0 AND temp <= 10 - külm
+//temp >=10 AND <15 Okei
+//temp >=15 and <20 Soe
+// >=20 <30 Kuum
+// >=30 Väga kuum
+
+Console.WriteLine("Sisesta temperatuur: ");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp >= 30)
+{
+    Console.WriteLine("Väga kuum");
+}
+else if (temp < 30 && temp >= 20)
+{
+    Console.WriteLine("Kuum");
+}
+else if (temp < 20 && temp >= 15)
+{
+    Console.WriteLine("Soe");
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine("Jahe");
+}
+else if (temp < 10 && temp > 0)
+{
+    Console.WriteLine("Külm");
+}
+else
+{
+    Console.WriteLine("Väga külm");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿//AND - &&
+
+//ture && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
